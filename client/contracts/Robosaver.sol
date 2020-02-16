179c27e6fe26d6262e921ee726e2983a2197bb35
Zacharias Mitzelos <accounts@mitzelos.com>
2020-02-15 18:08:17 -0700

    Add Robosaver contract and ABI

---
+++ client/contracts/Robosaver.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.5.12;
+
+contract Erc20 {
+    function approve(address, uint) external returns (bool);
+    function transfer(address, uint) external returns (bool);
+    function balanceOf(address) public view returns (uint);
+}
+
+contract CErc20 {
+    function mint(uint) external returns (uint);
+}
+
+contract CEth {
+    function mint() external payable;
+    function redeem(uint redeemTokens) public returns (uint);
+
+}
+
+contract Robosaver {
+    //Compound addresses for Ropsten
+    address payable ctbtcAddress = 0xB40d042a65Dd413Ae0fd85bECF8D722e16bC46F1;
+    address payable tbtcErc20Address = 0x083f652051b9CdBf65735f98d83cc329725Aa957;
+    address payable cethAddress = 0x1d70B01A2C3e3B2e56FcdcEfe50d5c5d70109a5D;
+
+    function supplyEthToCompound() public payable returns (bool) {
+        CEth(cethAddress).mint.value(msg.value).gas(250000)();
+        return true;
+    }
+
+    function withdrawAllFromCompound() public returns (uint){
+        Erc20 underlying = Erc20(tbtcErc20Address);
+        return CEth(ctbtcAddress).redeem(underlying.balanceOf(address(this)));
+    }
+
+    function supplyErc20ToCompound(uint256 _numTokensToSupply) public returns (uint) {
+        Erc20 underlying = Erc20(tbtcErc20Address);
+        CErc20 cToken = CErc20(ctbtcAddress);
+        underlying.approve(ctbtcAddress, _numTokensToSupply);
+        uint mintResult = cToken.mint(_numTokensToSupply);
+        return mintResult;
+    }
+
+    function transferOut(address _usersAddress, uint _amount) public payable returns(bool) {
+        Erc20 underlying = Erc20(tbtcErc20Address);
+        return underlying.transfer(_usersAddress, _amount);
+    }
+
+    function () external payable{}
+}
